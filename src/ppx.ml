1149382bdd9ec1e3f3a15cd943e59cea8fc8984d
mike dupont <mike.dupont@introspector.local>
2024-01-10 06:34:17 -0500

    adding dump

--- src/ppx.ml
+++ src/ppx.ml
@@ -1,19 +1,5 @@
 open Ppxlib
 
-(* let expander ~loc ~path:a b = *)
-(*   (print_endline (Batteries.dump ("DEBUG:OLDRULE1",a,b))); *)
-(*   { *)
-(*   pexp_desc = Pexp_constant (Pconst_string ("foo", loc, None)); *)
-(*   pexp_loc = loc; *)
-(*   pexp_attributes = []; *)
-(*   pexp_loc_stack = []; *)
-(* } *)
-
-(* let pattern = Ast_pattern.__ *)
-(* let extension = *)
-(*   Context_free.Rule.extension *)
-(*     (Extension.declare "yay" Expression pattern expander) *)
-
 (*: (Astlib.Ast_500.Parsetree.structure_item list) -> (Astlib.Ast_500.Parsetree.structure_item list)
 *)
 
@@ -40,9 +26,24 @@ let proc2 x  : unit =
   ()
 
 let printdesc2(x :structure_item_desc) :unit =
-  (print_endline (Batteries.dump ("DEBUG21:", x)))
+  (print_endline (Batteries.dump ("DEBUG33:", x)))
 
-
+(* let print_value_binding_expr_desc (x : expression_desc) : unit = *)
+(*   () *)
+  
+  
+let print_value_binding_expr (x : expression) : unit=
+  match x with
+  | {
+    pexp_desc (* : expression_desc *);
+    pexp_loc (* : location  *);
+    pexp_loc_stack (* : location_stack *);
+    pexp_attributes (* : attributes *); (* [... \[@id1\] \[@id2\]] *)
+  } ->
+    (print_endline (Batteries.dump ("DEBUG66:desc", pexp_desc )));
+    (print_endline (Batteries.dump ("DEBUG66:desc", pexp_attributes )));
+  ()
+  
 let print_value_binding_list2 (x : value_binding) : unit=
   match x with
   | {
@@ -51,7 +52,11 @@ let print_value_binding_list2 (x : value_binding) : unit=
     pvb_attributes; (* : attributes; *)
     pvb_loc; (* : location; *)
   } ->
-    (print_endline (Batteries.dump ("DEBUG21:", pvb_pat)))
+    (print_endline (Batteries.dump ("DEBUG55:pat", pvb_pat )));
+    (print_endline (Batteries.dump ("DEBUG55:expr", pvb_expr )));
+    (*print_value_binding_expr pvb_expr*)
+    (print_endline (Batteries.dump ("DEBUG55:atrr", pvb_attributes )));
+    (print_endline (Batteries.dump ("DEBUG55:loc", pvb_loc )))
 
 let rec print_value_binding_list (x : value_binding list) : unit=
   match x with
@@ -61,28 +66,202 @@ let rec print_value_binding_list (x : value_binding list) : unit=
     (print_value_binding_list t);
     ()
 
-  
-let printdesc(x :structure_item_desc) :unit =
-  (print_endline (Batteries.dump ("DEBUG21:", x)));
+let rec process_record_kind(x) =
   match x with
-  | Pstr_eval (expression,attributes) -> (print_endline (Batteries.dump ("Pstr_eval:", expression,attributes)))
+    {
+     pld_name(* : string loc *);
+     pld_mutable(* : mutable_flag *);
+     pld_type(* : core_type *);
+     pld_loc(* : Location.t *);
+     pld_attributes(* : attributes *); 
+   } ->
+    (print_endline (Batteries.dump ("precord_kind:",  
+                                    pld_name,
+                                    "mutable",
+                                    pld_mutable,
+                                    "type",
+                                    pld_type)))
+and
+process_core_type_desc x =
+  match x with
+  | Ptyp_any  -> (print_endline (Batteries.dump ("DEBUG:Ptyp_any")))
+  | Ptyp_var name ->(print_endline (Batteries.dump ("DEBUG:Ptyp_var"  , name)))
+  | Ptyp_arrow (arg_label , core_type , core_type2) ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow10" )))
+  | Ptyp_tuple a (* of core_type list *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow9" )))
+  | Ptyp_constr (a,b) (* of Longident.t loc * core_type list *)
+    ->
+    (process_core_type_list b);
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_constr", "id",a, "types",b )))
+  | Ptyp_object (a,b)(* of object_field list * closed_flag *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow8" )))
+  | Ptyp_class (a,b) (* of Longident.t loc * core_type list *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow7" )))
+  | Ptyp_alias (a,b) (* of core_type * string loc  *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow6" )))
+  | Ptyp_variant (a,b,c) (* of row_field list * closed_flag * label list option *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow5" )))
+  | Ptyp_poly (a,b) (* of string loc list * core_type *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow4" )))
+  | Ptyp_package a(* of package_type  *)
+    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow3",a )))    
+  (* | Ptyp_open (a,b) (\* of Longident.t loc * core_type *\)-> *)
+  (*   (print_endline (Batteries.dump ("DEBUG:Ptyp_arrow2",a,b ))) *)    
+  | Ptyp_extension a (* of extension   *)    ->
+    (print_endline (Batteries.dump ("DEBUG:Ptyp_extension",a )))
+and
+  process_record_kind_list(x) =
+  match x with
+  | [] -> ()
+  | h :: t ->
+    (process_record_kind h);
+    (process_record_kind_list t);
+    ()
+and
+process_core_type(x) =
+  match x with 
+    {
+      ptyp_desc(* : core_type_desc *);
+     ptyp_loc(* : Location.t *);
+     ptyp_loc_stack(* : location_stack *);
+     ptyp_attributes(* : attributes; *)
+    }->
+    (process_core_type_desc ptyp_desc);
+    (print_endline (Batteries.dump ("ptyp_desc"  , ptyp_desc)))
+and process_core_type_list(x:core_type list) =
+  match x with
+  | [] -> ()
+  | h :: t ->
+    process_core_type (h);
+    process_core_type_list(t)
+
+
+
+    
+
+    
+let print_constructor_arguments(x) =
+  match x with
+  | Pcstr_tuple a ->
+    (process_core_type_list a);
+    (print_endline (Batteries.dump ("DEBUG:Pcstr_tuple"  , a)))
+      
+  | Pcstr_record a ->
+    (print_endline (Batteries.dump ("DEBUG:Pcstr_record"  , a)))
+
+let rec process_Ptype_variant_constructor_declaration_list(x) =
+  match x with
+  | [] -> ()
+  | h :: t ->
+    (* (process_Ptype_variant_constructor_declaration_list h); *)
+    match h with
+    |{
+      pcd_name(* : string loc *);
+      pcd_vars(* : string loc list *);
+      pcd_args(* : constructor_arguments *);
+      pcd_res(* : core_type option *);
+      pcd_loc(* : Location.t *);
+      pcd_attributes(* : attributes *); 
+    }->
+      (* let name = match pcd_name with *)
+      (*   | (str,_) -> str *)
+      print_constructor_arguments(pcd_args);
+      (print_endline (Batteries.dump (
+           "constructor_declaration:",
+               pcd_name,
+               "vars",
+               pcd_vars,
+               "args",
+               pcd_args,
+               "res",
+               pcd_res,
+               "loc",
+               pcd_loc,
+               "attrs",
+               pcd_attributes
+             )));
+      (process_Ptype_variant_constructor_declaration_list t);
+      ()
+  
+let process_kind(x) =
+  match x with
+  (*and type_kind =*)
+  | Ptype_abstract  -> (print_endline (Batteries.dump ("DEBUG:Ptype_abstract:")))
+  | Ptype_variant a ->
+    (process_Ptype_variant_constructor_declaration_list a);
+    (print_endline (Batteries.dump ("DEBUG:Ptype_variant:",  a))) 
+    (*of constructor_declaration list *)
+    
+   | Ptype_record a ->     
+     process_record_kind_list(a)
+   | Ptype_open -> (print_endline (Batteries.dump ("DEBUG:Ptype_abstract:")))
+
+let print_type_decl(x) =
+  match x with
+    {
+      ptype_name (* : string loc *);
+      ptype_params (* : (core_type * (variance * injectivity)) list *);
+      ptype_cstrs (*: (core_type * core_type * location) list*) ;   
+      ptype_kind (*: type_kind*)  ; 
+      ptype_private (*: private_flag*); 
+      ptype_manifest (* : core_type option *);
+      ptype_attributes (*: attributes*);
+      ptype_loc (*: location*)
+    } ->
+    (print_endline (Batteries.dump ("type_decl:", ptype_name)));
+    (print_endline (Batteries.dump ("parameters:", ptype_params)));
+    (print_endline (Batteries.dump ("cstrs:", ptype_cstrs)));
+    (print_endline (Batteries.dump ("kind:",ptype_kind)));
+    (process_kind ptype_kind);
+    (print_endline (Batteries.dump ("private:",  ptype_private,
+                                    "manifest", ptype_manifest,
+                                    "attr", ptype_attributes,
+                                    "loc", ptype_loc
+                                   )))
+    
+let rec process_type_decl_list(x) =
+  match x with
+  | [] -> ()
+  | h :: t ->
+    (print_type_decl h);
+    (process_type_decl_list t);
+    ()
+    
+let printdesc(x :structure_item_desc) :unit =
+  (print_endline (Batteries.dump ("DEBUG44:", x)));
+  match x with
+  | Pstr_eval (expression,attributes) -> (print_endline (Batteries.dump ("DEBUG:Pstr_eval:", expression,attributes)))
   (*value binding*)
-  | Pstr_value (rec_flag, value_binding_list) ->(print_endline (Batteries.dump ("Pstr_value:", rec_flag, value_binding_list)));
+  | Pstr_value (rec_flag, value_binding_list) ->(print_endline (Batteries.dump ("DEBUG:Pstr_value:", rec_flag, value_binding_list)));
     print_value_binding_list(value_binding_list)
-  | Pstr_primitive value_description ->(print_endline (Batteries.dump ("Pstr_primitive:", value_description)))
-  | Pstr_type (rec_flag, type_declaration_list) ->(print_endline (Batteries.dump ("Pstr_type:", rec_flag, type_declaration_list)))
-  | Pstr_typext  type_extension ->(print_endline (Batteries.dump ("Pstr_typext:", type_extension)))
-  | Pstr_exception extension_constructor ->(print_endline (Batteries.dump ("Pstr_exception:", extension_constructor)))
-  | Pstr_module  module_binding ->(print_endline (Batteries.dump ("Pstr_module:",module_binding)))
-  | Pstr_recmodule  module_binding_list ->(print_endline (Batteries.dump ("Pstr_recmodule:", module_binding_list)))
-  | Pstr_modtype module_type_declaration ->(print_endline (Batteries.dump ("Pstr_modtype:", module_type_declaration)))
+  | Pstr_primitive value_description ->(print_endline (Batteries.dump ("DEBUG:Pstr_primitive:", value_description)))
+
+  | Pstr_type (rec_flag, type_declaration_list) ->
+    (*for expression_desc*)
+    process_type_decl_list(type_declaration_list);
+    (print_endline (Batteries.dump ("DEBUG:Pstr_type:", rec_flag, type_declaration_list)))
+
+
+  | Pstr_typext  type_extension ->(print_endline (Batteries.dump ("DEBUG:Pstr_typext:", type_extension)))
+  | Pstr_exception extension_constructor ->(print_endline (Batteries.dump ("DEBUG:Pstr_exception:", extension_constructor)))
+  | Pstr_module  module_binding ->(print_endline (Batteries.dump ("DEBUG:Pstr_module:",module_binding)))
+  | Pstr_recmodule  module_binding_list ->(print_endline (Batteries.dump ("DEBUG:Pstr_recmodule:", module_binding_list)))
+  | Pstr_modtype module_type_declaration ->(print_endline (Batteries.dump ("DEBUG:Pstr_modtype:", module_type_declaration)))
   (*open model*)
-  | Pstr_open open_description ->(print_endline (Batteries.dump ("Pstr_open", open_description)))
-  | Pstr_class (class_declarations ) ->(print_endline (Batteries.dump ("Pstr_class:", class_declarations)))
-  | Pstr_class_type (class_type_declarations) ->(print_endline (Batteries.dump ("Pstr_class_type:", class_type_declarations)))
-  | Pstr_include  (include_declaration)->(print_endline (Batteries.dump ("Pstr_include:",include_declaration)))
-  | Pstr_attribute (attribute)->(print_endline (Batteries.dump ("Pstr_attribute:", attribute)))
-  | Pstr_extension ( extension , attributes)->(print_endline (Batteries.dump ("Pstr_extension:", extension , attributes)))
+  | Pstr_open open_description ->(print_endline (Batteries.dump ("DEBUG:Pstr_open", open_description)))
+  | Pstr_class (class_declarations ) ->(print_endline (Batteries.dump ("DEBUG:Pstr_class:", class_declarations)))
+  | Pstr_class_type (class_type_declarations) ->(print_endline (Batteries.dump ("DEBUG:Pstr_class_type:", class_type_declarations)))
+  | Pstr_include  (include_declaration)->(print_endline (Batteries.dump ("DEBUG:Pstr_include:",include_declaration)))
+  | Pstr_attribute (attribute)->(print_endline (Batteries.dump ("DEBUG:Pstr_attribute:", attribute)))
+  | Pstr_extension ( extension , attributes)->(print_endline (Batteries.dump ("DEBUG:Pstr_extension:", extension , attributes)))
 
 let foo = 1
   
--- test/test.ml
+++ test/test.ml
@@ -3,7 +3,6 @@ open Ppxlib
     
 let loc = Location.none
 
-
 module Ast = Ast
 module Ast_helper = Ast_helper
 module Ast_magic = Selected_ast.Ast.Config
@@ -29,3 +28,115 @@ let test () =
 
 let () =
   run "Simple ppx test suit" [ ("Transform", [ ("Test", `Quick, test) ]) ]
+
+type expression_desc =
+  | Pexp_ident of longident_loc  (** Identifiers such as [x] and [M.x] *)
+  | Pexp_constant of constant
+      (** Expressions constant such as [1], ['a'], ["true"], [1.0], [1l], [1L],
+          [1n] *)
+  | Pexp_let of rec_flag * value_binding list * expression
+      (** [Pexp_let(flag, \[(P1,E1) ; ... ; (Pn,En)\], E)] represents:
+          - [let P1 = E1 and ... and Pn = EN in E] when [flag] is
+            {{!Asttypes.rec_flag.Nonrecursive} [Nonrecursive]},
+          - [let rec P1 = E1 and ... and Pn = EN in E] when [flag] is
+            {{!Asttypes.rec_flag.Recursive} [Recursive]}. *)
+  | Pexp_function of cases  (** [function P1 -> E1 | ... | Pn -> En] *)
+  | Pexp_fun of arg_label * expression option * pattern * expression
+      (** [Pexp_fun(lbl, exp0, P, E1)] represents:
+          - [fun P -> E1] when [lbl] is {{!Asttypes.arg_label.Nolabel}
+            [Nolabel]} and [exp0] is [None]
+          - [fun ~l:P -> E1] when [lbl] is {{!Asttypes.arg_label.Labelled}
+            [Labelled l]} and [exp0] is [None]
+          - [fun ?l:P -> E1] when [lbl] is {{!Asttypes.arg_label.Optional}
+            [Optional l]} and [exp0] is [None]
+          - [fun ?l:(P = E0) -> E1] when [lbl] is
+            {{!Asttypes.arg_label.Optional} [Optional l]} and [exp0] is
+            [Some E0]
+          Notes:
+          - If [E0] is provided, only {{!Asttypes.arg_label.Optional}
+            [Optional]} is allowed.
+          - [fun P1 P2 .. Pn -> E1] is represented as nested
+            {{!expression_desc.Pexp_fun} [Pexp_fun]}.
+          - [let f P = E] is represented using {{!expression_desc.Pexp_fun}
+            [Pexp_fun]}. *)
+  | Pexp_apply of expression * (arg_label * expression) list
+      (** [Pexp_apply(E0, \[(l1, E1) ; ... ; (ln, En)\])] represents
+          [E0 ~l1:E1 ... ~ln:En]
+          [li] can be {{!Asttypes.arg_label.Nolabel} [Nolabel]} (non labeled
+          argument), {{!Asttypes.arg_label.Labelled} [Labelled]} (labelled
+          arguments) or {{!Asttypes.arg_label.Optional} [Optional]} (optional
+          argument).
+          Invariant: [n > 0] *)
+  | Pexp_match of expression * cases
+      (** [match E0 with P1 -> E1 | ... | Pn -> En] *)
+  | Pexp_try of expression * cases
+      (** [try E0 with P1 -> E1 | ... | Pn -> En] *)
+  | Pexp_tuple of expression list
+      (** Expressions [(E1, ..., En)]
+          Invariant: [n >= 2] *)
+  | Pexp_construct of longident_loc * expression option
+      (** [Pexp_construct(C, exp)] represents:
+          - [C] when [exp] is [None],
+          - [C E] when [exp] is [Some E],
+          - [C (E1, ..., En)] when [exp] is [Some (Pexp_tuple\[E1;...;En\])] *)
+  | Pexp_variant of label * expression option
+      (** [Pexp_variant(`A, exp)] represents
+          - [`A] when [exp] is [None]
+          - [`A E] when [exp] is [Some E] *)
+  | Pexp_record of (longident_loc * expression) list * expression option
+      (** [Pexp_record(\[(l1,P1) ; ... ; (ln,Pn)\], exp0)] represents
+          - [{ l1=P1; ...; ln=Pn }] when [exp0] is [None]
+          - [{ E0 with l1=P1; ...; ln=Pn }] when [exp0] is [Some E0]
+          Invariant: [n > 0] *)
+  | Pexp_field of expression * longident_loc  (** [E.l] *)
+  | Pexp_setfield of expression * longident_loc * expression
+      (** [E1.l <- E2] *)
+  | Pexp_array of expression list  (** [\[| E1; ...; En |\]] *)
+  | Pexp_ifthenelse of expression * expression * expression option
+      (** [if E1 then E2 else E3] *)
+  | Pexp_sequence of expression * expression  (** [E1; E2] *)
+  | Pexp_while of expression * expression  (** [while E1 do E2 done] *)
+  | Pexp_for of pattern * expression * expression * direction_flag * expression
+      (** [Pexp_for(i, E1, E2, direction, E3)] represents:
+          - [for i = E1 to E2 do E3 done] when [direction] is
+            {{!Asttypes.direction_flag.Upto} [Upto]}
+          - [for i = E1 downto E2 do E3 done] when [direction] is
+            {{!Asttypes.direction_flag.Downto} [Downto]} *)
+  | Pexp_constraint of expression * core_type  (** [(E : T)] *)
+  | Pexp_coerce of expression * core_type option * core_type
+      (** [Pexp_coerce(E, from, T)] represents
+          - [(E :> T)] when [from] is [None],
+          - [(E : T0 :> T)] when [from] is [Some T0]. *)
+  | Pexp_send of expression * label loc  (** [E # m] *)
+  | Pexp_new of longident_loc  (** [new M.c] *)
+  | Pexp_setinstvar of label loc * expression  (** [x <- 2] *)
+  | Pexp_override of (label loc * expression) list
+      (** [{< x1 = E1; ...; xn = En >}] *)
+  | Pexp_letmodule of string option loc * module_expr * expression
+      (** [let module M = ME in E] *)
+  | Pexp_letexception of extension_constructor * expression
+      (** [let exception C in E] *)
+  | Pexp_assert of expression
+      (** [assert E].
+          Note: [assert false] is treated in a special way by the type-checker. *)
+  | Pexp_lazy of expression  (** [lazy E] *)
+  | Pexp_poly of expression * core_type option
+      (** Used for method bodies.
+          Can only be used as the expression under
+          {{!class_field_kind.Cfk_concrete} [Cfk_concrete]} for methods (not
+          values). *)
+  | Pexp_object of class_structure  (** [object ... end] *)
+  | Pexp_newtype of string loc * expression  (** [fun (type t) -> E] *)
+  | Pexp_pack of module_expr
+      (** [(module ME)].
+          [(module ME : S)] is represented as
+          [Pexp_constraint(Pexp_pack ME, Ptyp_package S)] *)
+  | Pexp_open of open_declaration * expression
+      (** - [M.(E)]
+          - [let open M in E]
+          - [let open! M in E] *)
+  | Pexp_letop of letop
+      (** - [let* P = E0 in E1]
+          - [let* P0 = E00 and* P1 = E01 in E1] *)
+  | Pexp_extension of extension  (** [\[%id\]] *)
+  | Pexp_unreachable  (** [.] *)
